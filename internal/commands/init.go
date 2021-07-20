b451bfbffd54180a42545c31605d83c78128ef31
mmadfox <sergey.liskonog@gmail.com>
2021-07-20 06:48:06 +0300

    update

--- internal/commands/init.go
+++ internal/commands/init.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/manifoldco/promptui"
+
 	"github.com/spf13/cobra"
 )
 
@@ -39,6 +41,87 @@ func validateArgsInitCommand(args []string) error {
 }
 
 func runInitCommand() error {
+
+	// project name
+	prompt := promptui.Prompt{
+		Label: "Project name",
+		Validate: func(s string) error {
+			if len(s) > 3 {
+				return nil
+			}
+			return fmt.Errorf("project name to short")
+		},
+	}
+	projectName, err := prompt.Run()
+	if err != nil {
+		return err
+	}
+
+	// stream name
+	prompt = promptui.Prompt{
+		Label:   "Stream name",
+		Default: projectName,
+		Validate: func(s string) error {
+			if len(s) > 3 {
+				return nil
+			}
+			return fmt.Errorf("stream name to short")
+		},
+	}
+	streamName, err := prompt.Run()
+	if err != nil {
+		return err
+	}
+
+	// go mod
+	prompt = promptui.Prompt{
+		Label:   "Go module (go.mod)",
+		Default: projectName,
+		Validate: func(s string) error {
+			if len(s) > 3 {
+				return nil
+			}
+			return fmt.Errorf("go.mod module to short")
+		},
+	}
+	goMod, err := prompt.Run()
+	if err != nil {
+		return err
+	}
+
+	// author
+	prompt = promptui.Prompt{
+		Label: "Author",
+	}
+	author, err := prompt.Run()
+	if err != nil {
+		return err
+	}
+
+	// email
+	prompt = promptui.Prompt{
+		Label: "Email",
+	}
+	email, err := prompt.Run()
+	if err != nil {
+		return err
+	}
+
+	// description
+	prompt = promptui.Prompt{
+		Label: "Description",
+	}
+	desc, err := prompt.Run()
+	if err != nil {
+		return err
+	}
+
+	_ = projectName
+	_ = streamName
+	_ = goMod
+	_ = author
+	_ = email
+	_ = desc
 
 	return nil
 }
