28a83d2e07caf2a0331c6f618e0038671fdce5e5
mmadfox <sergey.liskonog@gmail.com>
2021-07-26 08:20:18 +0300

    source: update

--- internal/source/validator.go
+++ internal/source/validator.go
@@ -1,12 +1,32 @@
 package source
 
-// projection
-// mutation
-// events
-// commands
-// stream
-// controllers
+import (
+	"path/filepath"
 
-func FilesValidation(path string, packageName string) error {
+	"github.com/go-gulfstream/gs/internal/schema"
+)
+
+var streamFiles = []string{
+	"/internal/projection/projection.go",
+	"/internal/projection/controller.go",
+	"/internal/stream/mutation.go",
+	"/internal/stream/controller.go",
+	"/internal/stream/state.go",
+	"/pkg/{events_package}/events.go",
+	"/pkg/{commands_package}/commands.go",
+}
+
+func iterStreamFiles(path string, m *schema.Manifest, fn func(filename string) error) error {
+	for _, filename := range streamFiles {
+		filename = schema.NormalizePath(filename, m)
+		filename = filepath.Join(path, filename)
+		if err := fn(filename); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func FilesValidation(path string, m *schema.Manifest) error {
 	return nil
 }
