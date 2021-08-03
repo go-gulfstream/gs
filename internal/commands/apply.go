e467505b2aee61f2f5157ab3f611d9d3bd3522ce
mmadfox <sergey.liskonog@gmail.com>
2021-08-03 04:14:08 +0300

    update

--- internal/commands/apply.go
+++ internal/commands/apply.go
@@ -92,8 +92,8 @@ func runApplyCommand(path string) error {
 				status = statusSkip
 				skip = true
 			}
-			fmt.Printf("%s %s{InCommand: %s, OutEvent: %s}, %s => %s\n", status,
-				m.Mutation, m.Command.Name, m.Event.Name, file.Addon, file.Path)
+			fmt.Printf("%s %s, %s => %s\n", status,
+				m.Mutation, file.Addon, file.Path)
 			if skip {
 				skipCounter++
 				return nil
@@ -117,8 +117,8 @@ func runApplyCommand(path string) error {
 				status = statusSkip
 				skip = true
 			}
-			fmt.Printf("%s %s{InEvent: %s, OutEvent: %s} => %s\n", status,
-				m.Mutation, m.InEvent.Name, m.OutEvent.Name, file.Path)
+			fmt.Printf("%s %s, %s => %s\n", status,
+				m.Mutation, file.Addon, file.Path)
 			if skip {
 				skipCounter++
 				return nil
